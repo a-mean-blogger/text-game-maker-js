4088752732d115818c3a7dfb0d050eaf364cbf60
Ian Hong <a.mean.blogger@gmail.com>
2019-02-04 14:58:03 -0800

    v1.0.2 TMS.cursor, TMI.keyboard.getText updated

--- src/default-settings/default-settings.js
+++ src/default-settings/default-settings.js
@@ -1,5 +1,5 @@
 var TM = {};
-TM.version = '1.0.1';
+TM.version = '1.0.2';
 TM.defaultSettings = {
   screen: {
     canvasId: 'tm-canvas',
@@ -8,10 +8,10 @@ TM.defaultSettings = {
     row: 20,
     backgroundColor: '#151617',
     webFontJsPath: 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js',
-    fontFaceObserverJsPath:'https://cdnjs.cloudflare.com/ajax/libs/fontfaceobserver/2.0.13/fontfaceobserver.js',
+    fontFaceObserverJsPath: 'https://cdnjs.cloudflare.com/ajax/libs/fontfaceobserver/2.0.13/fontfaceobserver.js',
     fontColor: '#F5F7FA',
-    fontFamily: 'monospace',
-    fontSource: null,
+    fontFamily: 'Nanum Gothic Coding',
+    fontSource: 'https://cdn.jsdelivr.net/font-nanum/1.0/nanumgothiccoding/nanumgothiccoding.css',
     fontSize: 30,
     zoom: 0.5,
   },
@@ -22,6 +22,13 @@ TM.defaultSettings = {
       sizeAdj: 1,
       xAdj: 0,
       yAdj: 0,
+    },
+    fullwidth: {// ■□★☆△▷▽◁┍┑┕┙│━┏┓┗┛━┃...
+      chars: '\u2008\u2022\u2190-\u21f0\u2500-\u2BFF',
+      isFullwidth: true,
+      sizeAdj: 1.2,
+      xAdj: -0.05,
+      yAdj: 0.03,
     },
   },
   debug: {
